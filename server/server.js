3bd14e0c5f954248fbe6f3d271982a2c479a1d65
Donny <107092000+Donny-Guo@users.noreply.github.com>
2026-03-02 17:52:16 -0500

    Fix nonsense

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 import express from "express"
+import path from "path"
 import giftsRouter from "./routes/gifts.js"
 
 const app = express();
 
 app.use("/public", express.static("./public"));
 
-app.use("/scripts", express.static("./public/scripts"));
+app.use(express.static("./public"));
 
 app.use("/gifts", giftsRouter);
 
-app.get("/", (req, res) => {
-  res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">UnEarthed API</h1>')
+app.use((req, res) => {
+  res.status(404).sendFile(path.resolve("./public/404.html"))
 });
 
 const PORT = process.env.PORT || 3001
